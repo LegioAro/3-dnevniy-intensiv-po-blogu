3cff98ca729f65e8f4229619bc982912a792601a
LegionAro <u@example.com>
2024-10-15 10:01:32 +0300

    v1.0

--- js/main.js
+++ js/main.js
@@ -43,3 +43,48 @@ function checkDivVisibility() {
 }
 
 document.addEventListener('scroll', checkDivVisibility);
+
+//Modal
+function isModal() {
+  let modalBtns = document.querySelectorAll('.modal__btn-active');
+
+  if (modalBtns.length > 0) {
+    for (let modalBtn of modalBtns) {
+      modalBtn.addEventListener('click', function () {
+        let modalBtnData = modalBtn.getAttribute('data-modal-src');
+        let modalWindow = document.querySelector(`*[data-modal-window="${modalBtnData}"]`);
+        let body = document.querySelector('body');
+
+        if (modalWindow) {
+          modalWindow.classList.add('active');
+          body.classList.add('lock');
+        }
+      });
+    }
+  }
+}
+isModal();
+
+function isModalClose() {
+  let modalCloseBtns = document.querySelectorAll('.modal__btn-close');
+  if (modalCloseBtns.length > 0) {
+    for (let modalCloseBtn of modalCloseBtns) {
+      modalCloseBtn.addEventListener('click', function () {
+        let modalWindow = modalCloseBtn.closest('*[data-modal-window]');
+        let body = document.querySelector('body');
+
+        modalWindow.classList.remove('active');
+        body.classList.remove('lock');
+      });
+    }
+  }
+}
+isModalClose();
+
+//resize
+
+isResize('.who__desc-img', '.who__desc', '.who__desc-mob', 768, 'first');
+
+window.addEventListener('resize', () => {
+  isResize('.who__desc-img', '.who__desc', '.who__desc-mob', 768, 'first');
+});

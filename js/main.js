d6ea4708be43234910c47472c1a77e4541d6e77a
LegionAro <u@example.com>
2024-11-02 17:31:09 +0200

    add timers

--- js/main.js
+++ js/main.js
@@ -88,3 +88,74 @@ isResize('.who__desc-img', '.who__desc', '.who__desc-mob', 768, 'first');
 window.addEventListener('resize', () => {
   isResize('.who__desc-img', '.who__desc', '.who__desc-mob', 768, 'first');
 });
+
+//timer
+function isTimer() {
+  let dateEnd = localStorage.getItem('dateEnd');
+  let dateNow = new Date();
+  let dateMinutes = dateNow.setMinutes(dateNow.getMinutes() + 24 * 60 * 60);
+
+  if (!dateEnd) {
+    localStorage.setItem('dateEnd', dateMinutes);
+    dateEnd = localStorage.getItem('dateEnd');
+  }
+
+  function countdownTimer() {
+    const diff = dateEnd - new Date();
+    if (diff <= 0) {
+      let dateNow = new Date();
+
+      localStorage.setItem('dateEnd', dateNow.setMinutes(dateNow.getMinutes() + 24 * 60 * 60));
+      dateEnd = localStorage.getItem('dateEnd');
+    }
+
+    let timers = document.querySelectorAll('.timer');
+
+    timers.forEach((timer) => {
+      let timerHours = timer.querySelector('[data-timer-hours]');
+      let timerMinuts = timer.querySelector('[data-timer-minuts]');
+      let timerSeconds = timer.querySelector('[data-timer-seconds]');
+      // let timerMilSec = timer.querySelector('[data-timer-milsec]');
+
+      let timerHoursItems = timerHours.querySelectorAll('.timer__item-num');
+      let timerMinutesItems = timerMinuts.querySelectorAll('.timer__item-num');
+      let timerSecondsItems = timerSeconds.querySelectorAll('.timer__item-num');
+      // let timerMilSecItems = timerMilSec.querySelectorAll('.timer__item-num');
+
+      const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+      const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+      const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+      // const milSeconds = diff > 0 ? Math.floor(diff) % 100 : 0;
+
+      let hoursString = hours < 10 ? '0' + hours : String(hours);
+      let minutesString = minutes < 10 ? '0' + minutes : String(minutes);
+      let secondsString = seconds < 10 ? '0' + seconds : String(seconds);
+      // let milsecString = milSeconds < 10 ? '0' + milSeconds : String(milSeconds);
+
+      let hoursArr = hoursString.split('');
+      let minutesArr = minutesString.split('');
+      let secondsArr = secondsString.split('');
+      // let milsecArr = milsecString.split('');
+
+      // for (let i = 0; i < timerMilSecItems.length; i++) {
+      //   timerMilSecItems[i].innerHTML = milsecArr[i];
+      // }
+      for (let i = 0; i < timerMinutesItems.length; i++) {
+        timerMinutesItems[i].innerHTML = minutesArr[i];
+      }
+      for (let i = 0; i < timerSecondsItems.length; i++) {
+        timerSecondsItems[i].innerHTML = secondsArr[i];
+      }
+      for (let i = 0; i < timerHoursItems.length; i++) {
+        timerHoursItems[i].innerHTML = hoursArr[i];
+      }
+
+      // вызываем функцию countdownTimer
+    });
+  }
+  countdownTimer();
+  // вызываем функцию countdownTimer каждую секунду
+  let timerId = setInterval(countdownTimer, 1000);
+}
+
+isTimer();
